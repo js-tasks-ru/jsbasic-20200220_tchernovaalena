4730325c46448452652910bca5ab4855d941b2df
Алёна Чернова <tchernovaalena@gmail.com>
2020-02-27 20:11:42 +0300

    assignment0assignment1.1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,23 @@
  * @returns {number}
  */
 function factorial(n) {
+    
+  let factorial = n;
+  
+  if (n != 0) {
+  	
+  	for (i = 1; i < n; i++) {
+  		factorial = factorial * i;
+  		}
+  	
+  	return factorial;
+	}
+  
+  else {
+  	return 1;
+  	}
+  }
+    
+  
 
-}
+
