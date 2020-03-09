57583bda707d959c063fefc524cea7a9371a5a57
Алёна Чернова <tchernovaalena@gmail.com>
2020-03-09 19:01:19 +0300

    Module-2-Task-1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,21 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
+'use strict'
+
 function sumSalary(salaries) {
+  
+  let sum = 0;
+
+  for (let key in salaries) {
+    
+    if (Number.isInteger(salaries[key])) {
+      sum = sum + salaries[key];
+    }
+    
+  }
+  
+  return sum;
 
 }
+
