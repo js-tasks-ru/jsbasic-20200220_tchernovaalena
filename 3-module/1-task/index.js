92268fb7b16263a9e54edaae5395dd99f347940d
Алёна Чернова <tchernovaalena@gmail.com>
2020-03-11 22:10:37 +0300

    Module-3-Task-1

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,18 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  let namesSalaries = '';
+  data.forEach(function(item, index, data) {
+    if ((item.age <= age)) {
+      if (namesSalaries === '') {
+        let newNameSalary = `${item.name}, ${item.balance}`;
+        namesSalaries = namesSalaries + newNameSalary;
+      }  
+      else {
+        let newNameSalary = `\n${item.name}, ${item.balance}`;
+        namesSalaries = namesSalaries + newNameSalary;
+      }
+    }
+  });
+  return namesSalaries;
 }
