a1a312d37a88f91e16d5ce966588a645e6342eef
Алёна Чернова <tchernovaalena@gmail.com>
2020-03-22 14:06:04 +0300

    Module-4-Task-1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,7 +1,21 @@
+// Add your code here
+'use strict';
+
 /**
  * Генерация HTML списка друзей
  * @param {Object[]} friends
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-}
+
+  let ul = document.createElement('ul');
+    
+    
+
+  for (let i = 0; i < friends.length; i++) {  
+    ul.innerHTML = ul.innerHTML + `<li>${friends[i].firstName} ${friends[i].lastName}</li>`;
+  }
+
+  return ul;
+} 
+
